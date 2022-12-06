a694ecc37c5f3903bcfc1e1a133ab70553efec15
aguilarklyno <aguilarklyno@gmail.com>
2022-12-06 20:39:34 +0900

    Progress

---
+++ 07-react-axios-yt/src/components/Weather.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=0a3108f2d27768196091cc0b9f5be048"
+
+
+const Weather = () => {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setData(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
+  console.log(data)
+
+  if(!data) return null;
+
+  return (
+    <div>
+      <p>{data.main.temp}</p>
+    </div>
+  )
+}
+
+export default Weather
